3f6651485a2914f768450e88468e52f89824f6db
Muhammad Aiman Furqon <68735242+aimanfurqon@users.noreply.github.com>
2023-05-08 17:21:52 +0800

    Added json + odd marked page in juz 25

--- src/assets/mushaf/juz25/index.js
+++ src/assets/mushaf/juz25/index.js
@@ -3,7 +3,7 @@ export default Data = () => ({
   metadata: require("assets/mushaf/juz25/content/data.json"),
   pages: {
     1: {
-      image: require("assets/mushaf/juz25/pages/481.png"),
+      image: require("assets/mushaf/juz25/pages/481m.png"),
       content: require("assets/mushaf/juz25/content/1.json"),
     },
     2: {
@@ -11,7 +11,7 @@ export default Data = () => ({
       content: require("assets/mushaf/juz25/content/2.json"),
     },
     3: {
-      image: require("assets/mushaf/juz25/pages/483.png"),
+      image: require("assets/mushaf/juz25/pages/483m.png"),
       content: require("assets/mushaf/juz25/content/3.json"),
     },
     4: {
@@ -19,7 +19,7 @@ export default Data = () => ({
       content: require("assets/mushaf/juz25/content/4.json"),
     },
     5: {
-      image: require("assets/mushaf/juz25/pages/485.png"),
+      image: require("assets/mushaf/juz25/pages/485m.png"),
       content: require("assets/mushaf/juz25/content/5.json"),
     },
     6: {
@@ -27,56 +27,56 @@ export default Data = () => ({
       content: require("assets/mushaf/juz25/content/6.json"),
     },
     7: {
-      image: require("assets/mushaf/juz25/pages/487.png"),
-      content: require("assets/mushaf/juz30/content/7.json"),
+      image: require("assets/mushaf/juz25/pages/487m.png"),
+      content: require("assets/mushaf/juz25/content/7.json"),
     },
     8: {
       image: require("assets/mushaf/juz25/pages/488-marked_01.png"),
       content: require("assets/mushaf/juz25/content/8.json"),
     },
     9: {
-      image: require("assets/mushaf/juz25/pages/489.png"),
-      content: require("assets/mushaf/juz30/content/9.json"),
+      image: require("assets/mushaf/juz25/pages/489m.png"),
+      content: require("assets/mushaf/juz25/content/9.json"),
     },
     10: {
       image: require("assets/mushaf/juz25/pages/490.png"),
       content: require("assets/mushaf/juz25/content/10.json"),
     },
     11: {
-      image: require("assets/mushaf/juz25/pages/491.png"),
-      content: require("assets/mushaf/juz30/content/11.json"),
+      image: require("assets/mushaf/juz25/pages/491m.png"),
+      content: require("assets/mushaf/juz25/content/11.json"),
     },
     12: {
       image: require("assets/mushaf/juz25/pages/492.png"),
       content: require("assets/mushaf/juz25/content/12.json"),
     },
     13: {
-      image: require("assets/mushaf/juz25/pages/493.png"),
-      content: require("assets/mushaf/juz30/content/13.json"),
+      image: require("assets/mushaf/juz25/pages/493m.png"),
+      content: require("assets/mushaf/juz25/content/13.json"),
     },
     14: {
       image: require("assets/mushaf/juz25/pages/494.png"),
       content: require("assets/mushaf/juz25/content/14.json"),
     },
     15: {
-      image: require("assets/mushaf/juz25/pages/495.png"),
-      content: require("assets/mushaf/juz30/content/15.json"),
+      image: require("assets/mushaf/juz25/pages/495m.png"),
+      content: require("assets/mushaf/juz25/content/15.json"),
     },
     16: {
       image: require("assets/mushaf/juz25/pages/496m.png"),
       content: require("assets/mushaf/juz25/content/16.json"),
     },
     17: {
-      image: require("assets/mushaf/juz25/pages/497.png"),
-      content: require("assets/mushaf/juz30/content/17.json"),
+      image: require("assets/mushaf/juz25/pages/497m.png"),
+      content: require("assets/mushaf/juz25/content/17.json"),
     },
     18: {
       image: require("assets/mushaf/juz25/pages/498-marked_0.png"),
       content: require("assets/mushaf/juz25/content/18.json"),
     },
     19: {
-      image: require("assets/mushaf/juz25/pages/499.png"),
-      content: require("assets/mushaf/juz30/content/19.json"),
+      image: require("assets/mushaf/juz25/pages/499m.png"),
+      content: require("assets/mushaf/juz25/content/19.json"),
     },
     20: {
       image: require("assets/mushaf/juz25/pages/500-marked_0.png"),
